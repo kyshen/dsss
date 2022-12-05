06154071ca8f28d3b125b4c271b61138ebd8e38f
Kaiyu Shen <shen_kaiyu@163.com>
2022-12-05 21:37:10 +0800

    finish costats

--- costas1.m
+++ costas1.m
@@ -1,31 +1,33 @@
-clc; clear; close all; 
+%clc; clear; close all; 
 %close all; 
 %定义锁相环的工作模式：单载波为“1”、BPSK调制为“2”、QPSK调制为“3” 
 PLL_Mode = 2; 
-%仿真数据长度 
-L=10000; 
+%仿真数据长度  
+fs4costas=3500e7;
+t4costas=0:1/fs4costas:time-1/fs4costas; % 时间序列
+L=length(t4costas);
 %基带信号 
-if PLL_Mode == 1 
-    I_Data=ones(L,1); 
-    Q_Data=I_Data;
-end
-if PLL_Mode == 2
-    I_Data=(randi(2,L,1)-2)*2+1; 
-    Q_Data=zeros(1,L,1); 
-end
-if PLL_Mode == 3
-    I_Data=(randi(2,L,1)-2)*2+1; 
-    Q_Data=(randi(2,L,1)-2)*2+1;
-end
+% if PLL_Mode == 1 
+%     I_Data=ones(L,1); 
+%     Q_Data=I_Data;
+% end
+% if PLL_Mode == 2
+%     I_Data=(randi(2,L,1)-2)*2+1; 
+%     Q_Data=zeros(1,L,1)'; 
+% end
+% if PLL_Mode == 3
+%     I_Data=(randi(2,L,1)-2)*2+1; 
+%     Q_Data=(randi(2,L,1)-2)*2+1;
+% end
 
+I_Data=(randi(2,L,1)-2)*2+1; 
+Q_Data=zeros(1,L,1)'; 
 Signal_Source=I_Data + 1i*Q_Data; 
 
-%载波信号 
-Freq_Sample=2400;%采样率，Hz 
-Delta_Freq=-60; %频偏，Hz 
-Time_Sample=1/Freq_Sample; 
-Delta_Phase=rand(1)*2*pi; %随机初相，Rad 
-Carrier=exp(1i*(Delta_Freq/Freq_Sample*(1:L)+Delta_Phase)); 
+
+Delta_Freq=fc+fd; %频偏，Hz 
+Delta_Phase=0; %随机初相，Rad 
+Carrier=exp(1i*(2*pi*Delta_Freq*t4costas+Delta_Phase)); 
 %调制处理 
 Signal_Channel=Signal_Source.*Carrier'; 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
@@ -65,64 +67,82 @@ for i=2:L
     NCO_Phase(i)=NCO_Phase(i-1)+Freq_Control(i); 
 end 
 %画图显示结果 
-figure
-plot(Freq_Control(2:L)*Freq_Sample) 
-figure 
-plot(cos(NCO_Phase),'r') 
-hold on 
-plot(real(Carrier)) 
-figure 
-subplot(2,2,1) 
-plot(-PLL_Freq_Part(2:L)*Freq_Sample); 
-grid on; 
-title('锁相环频率响应曲线'); 
-axis([1 L -100 100]); 
-subplot(2,2,2) 
-plot(PLL_Phase_Part(2:L)*180/pi); 
-title('锁相环相位响应曲线'); 
-axis([1 L -2 2]); 
-grid on; 
-%设定显示范围 
-Show_D=300; %起始位置 
-Show_U=900; %终止位置 
-Show_Length=Show_U-Show_D; 
-subplot(2,2,3) 
-plot(Signal_Channel(Show_D:Show_U),'*'); 
-title('进入锁相环的数据星座图'); 
-axis([-2 2 -2 2]); 
-grid on; 
-hold on; 
-subplot(2,2,3) 
-plot(Signal_PLL(Show_D:Show_U),'r*'); 
-grid on; 
-subplot(2,2,4) 
-plot(Signal_PLL(Show_D:Show_U),'r*'); 
-title('锁相环锁定及稳定后的数据星座图'); 
-axis([-2 2 -2 2]); 
-grid on; 
- 
-figure 
-%设定显示范围 
-Show_D=300; %起始位置 
-Show_U=350; %终止位置 
-Show_Length=Show_U-Show_D; 
-subplot(2,2,1) 
-plot(I_Data(Show_D:Show_U)); 
-grid on; 
-title('I路信息数据'); 
-axis([1 Show_Length -2 2]); 
-subplot(2,2,2) 
-plot(Q_Data(Show_D:Show_U)); 
-grid on; 
-title('Q路信息数据'); 
-axis([1 Show_Length -2 2]); 
-subplot(2,2,3) 
-plot(I_PLL(Show_D:Show_U)); 
-grid on; 
-title('锁相环输出I路信息数据'); 
-axis([1 Show_Length -2 2]); 
-subplot(2,2,4) 
-plot(Q_PLL(Show_D:Show_U)); 
-grid on; 
-title('锁相环输出Q路信息数据'); 
-axis([1 Show_Length -2 2]); 
+% figure
+% plot(Freq_Control(2:L)*Freq_Sample,'r') 
+
+% figure (1)
+% plot(cos(NCO_Phase),'b') 
+% hold on 
+% plot(real(Carrier))
+% axis([L-10000 L -2 2])
+% hold off
+
+
+
+% figure (2)
+% plot(carrier_T,'b') 
+% hold on 
+% plot(cos(NCO_Phase))
+% axis([L-10000 L -2 2])
+% hold off
+
+% figure 
+% plot(real(Carrier),'b') 
+% hold on 
+% plot(carrier_T)
+% figure 
+
+% subplot(2,2,1) 
+% plot(-PLL_Freq_Part(2:L)*Freq_Sample); 
+% grid on; 
+% title('锁相环频率响应曲线'); 
+% axis([1 L -100 100]); 
+% subplot(2,2,2) 
+% plot(PLL_Phase_Part(2:L)*180/pi); 
+% title('锁相环相位响应曲线'); 
+% axis([1 L -2 2]); 
+% grid on; 
+% %设定显示范围 
+% Show_D=300; %起始位置 
+% Show_U=900; %终止位置 
+% Show_Length=Show_U-Show_D; 
+% subplot(2,2,3) 
+% plot(Signal_Channel(Show_D:Show_U),'*'); 
+% title('进入锁相环的数据星座图'); 
+% axis([-2 2 -2 2]); 
+% grid on; 
+% hold on; 
+% subplot(2,2,3) 
+% plot(Signal_PLL(Show_D:Show_U),'r*'); 
+% grid on; 
+% subplot(2,2,4) 
+% plot(Signal_PLL(Show_D:Show_U),'r*'); 
+% title('锁相环锁定及稳定后的数据星座图'); 
+% axis([-2 2 -2 2]); 
+% grid on; 
+%  
+% figure 
+% %设定显示范围 
+% Show_D=300; %起始位置 
+% Show_U=350; %终止位置 
+% Show_Length=Show_U-Show_D; 
+% subplot(2,2,1) 
+% plot(I_Data(Show_D:Show_U)); 
+% grid on; 
+% title('I路信息数据'); 
+% axis([1 Show_Length -2 2]); 
+% subplot(2,2,2) 
+% plot(Q_Data(Show_D:Show_U)); 
+% grid on; 
+% title('Q路信息数据'); 
+% axis([1 Show_Length -2 2]); 
+% subplot(2,2,3) 
+% plot(I_PLL(Show_D:Show_U)); 
+% grid on; 
+% title('锁相环输出I路信息数据'); 
+% axis([1 Show_Length -2 2]); 
+% subplot(2,2,4) 
+% plot(Q_PLL(Show_D:Show_U)); 
+% grid on; 
+% title('锁相环输出Q路信息数据'); 
+% axis([1 Show_Length -2 2]); 
--- test_Datacode.m
+++ test_Datacode.m
@@ -1,17 +1,12 @@
 %% 初始化设置
 clear,clc
-dfff_settings
+hold off
+close all
+dsss_settings
 v=5;
 %% 产生信息码、C/A码
 data=sign(rand(1,data_length)-0.5); % 信息码，正负1
 Datacode=sample(data,RB,fs,time);
-
-% ca=CAcodeGenerator([3,8]);
-% ca_shift=circshift(ca,CA_shift);
-% ca=2*ca-1;
-% ca_shift=2*ca_shift-1;
-% CAcode_T=sample(ca_shift,f_ca,fs,time); % 发送端C/A码
-% CAcode_R=sample(ca,f_ca,fs,time); % 接收端C/A码
 
 ca=CAcodeGenerator([3,8]);
 ca=2*ca-1;
@@ -33,19 +28,15 @@ CAcode_R_after_catch=CAcode_x((1:N)+n);
 carrier_R_after_catch=A*cos(2*pi*(fc+catched_fd(1))*t);
 
 Costas_track; % 信号跟踪
+
+NCO_Phase=resample(NCO_Phase,N,length(NCO_Phase));
 carrier_R_after_track=cos(NCO_Phase)';
 
 Early_Late_gate; % 早迟门
 CAcode_R_after_track=CAcode_x((1:N)+n+delta_n);
 
-% r_0=r_DS.*CAcode_R_after_track; % 解扩
-% r_1=filter(b1,a1,r_0);
-% r_2=r_1.*carrier_R_after_track; % 解调
-% m=filter(b2,a2,r_2);
-% plot(m)
-
 r_0=r_DS.*CAcode_R_after_track; % 解扩
 r_1=filter(b1,a1,r_0);
-r_2=r_1*A.*cos(2*pi*(fc+fd)*t)*2; % 解调
+r_2=-r_1.*carrier_R_after_track*2; % 解调
 Datacode_out=filter(b2,a2,r_2);
 plot(Datacode_out)
