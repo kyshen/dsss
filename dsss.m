3ac188b1e5ebbd1cf7aced6deb65f948fed14e1f
Kaiyu Shen <shen_kaiyu@163.com>
2022-12-11 21:55:10 +0800

    change into function form

--- dsss.m
+++ dsss.m
@@ -3,7 +3,6 @@ clear,clc
 hold off
 close all
 dsss_settings
-v=5;
 %% 产生信息码、C/A码
 data=sign(rand(1,data_length)-0.5); % 信息码，正负1
 Datacode=sample(data,RB,fs,time);
@@ -21,26 +20,28 @@ s_0=Datacode.*carrier_T; % 调制载波
 s_DS=s_0.*CAcode_T; % 扩频
 r_DS = awgn(s_DS,SNR); % 信道噪声
 
-FFT_catch; % 信号捕获
 
-n=floor(catched_CAcode_shift*(1/f_ca)*fs);
-CAcode_R_after_catch=CAcode_x((1:N)+n);
-carrier_R_after_catch=A*cos(2*pi*(fc+catched_fd)*t);
-
-Costas_track; % 信号跟踪
-
-NCO_Phase=resample(NCO_Phase,N,length(NCO_Phase));
-carrier_R_after_track=cos(NCO_Phase)';
-
-
-
-Early_Late_gate; % 早迟门
-CAcode_R_after_track=CAcode_x((1:N)+n+delta_n);
-
-r_0=r_DS.*CAcode_R_after_track; % 解扩
-r_1=filter(b1,a1,r_0);
-r_2=-r_1.*carrier_R_after_track*2; % 解调
-Datacode_out=filter(b2,a2,r_2);
+Datacode_out=zeros(1,N);
+for i=1
+    [catched_fd, catched_CAcode_shift] = FFT_catch(i); % 信号捕获
+    
+    n=floor(catched_CAcode_shift*(1/f_ca)*fs);
+    %CAcode_R_after_catch=CAcode_x((1:N)+n);
+    %carrier_R_after_catch=A*cos(2*pi*(fc+catched_fd)*t);
+    
+    NCO_Phase = Costas_track(i); % 信号跟踪
+    
+    carrier_R_after_track=cos(NCO_Phase)';
+    
+    delta_n = Early_Late_gate(i); % 早迟门
+    CAcode_R_after_track=CAcode_x((1:N_1)+(i-1)*N_1+n+delta_n);
+    
+    r_0=r_DS((1:N_1)+(i-1)*N_1).*CAcode_R_after_track; % 解扩
+    r_1=filter(b1,a1,r_0);
+    r_2=-r_1.*carrier_R_after_track*2; % 解调
+    Datacode_out_1=filter(b2,a2,r_2);
+    Datacode_out((1:N_1)+(i-1)*N_1)=Datacode_out_1;
+end
 
 %% 结果
 plot(Datacode_out)
